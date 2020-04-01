f4a0f7823d776a314fc3fec01cd1490012081d51
grsdev7 <grsdev7@gmail.com>
2020-04-01 16:22:43 +0200

    first commit after importing from spring initializer

---
+++ src/main/java/com/grsdev7/covid19/Covid19Soln1/Covid19Soln1Application.java
@@ -0,0 +1,13 @@
+package com.grsdev7.covid19.Covid19Soln1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19Soln1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19Soln1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grsdev7/covid19/Covid19Soln1/Covid19Soln1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grsdev7.covid19.Covid19Soln1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19Soln1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
